7518c50ca308eb6a2739ba1be617c8184622cfc0
buiuferro <bruno.ferro@fatec.sp.gov.br>
2021-04-23 16:56:05 -0300

    Add riot api info

--- src/constants/riotHeader.ts
+++ src/constants/riotHeader.ts
@@ -1,10 +1,10 @@
 const HEADER = {
-  'User-Agent':
+  user_agent:
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
-  'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-  'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-  Origin: 'https://developer.riotgames.com',
-  'X-Riot-Token': 'RGAPI-587d8590-d4dd-46ff-abde-f18559ac889f',
+  accept_language: 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+  accept_charset: 'application/x-www-form-urlencoded; charset=UTF-8',
+  origin: 'https://developer.riotgames.com',
+  x_riot_token: 'RGAPI-587d8590-d4dd-46ff-abde-f18559ac889f',
 };
 
 export default HEADER;
