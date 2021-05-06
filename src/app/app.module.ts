2607cd1d3638da4fb75ba0984e02ea81d8d1a08d
buiuferro <bruno.ferro@fatec.sp.gov.br>
2021-05-06 18:14:03 -0300

    mongoose intalled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { HealthModule } from './modules/health/health.module';
 import { HelloModule } from './modules/hello/hello.module';
 import { SummonerModule } from './modules/summoner/summoner.module';
 
+
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb+srv://buiuferro:lol-cluster@lol-cluster.bizxl.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
     HelloModule,
     HealthModule,
     SummonerModule,
